30573432d3c54da662df403a14fb4197d073d9af
Benno <bennington.schaab@gmail.com>
2018-05-10 18:59:32 +0200

    B

--- Test/R6.py
+++ Test/R6.py
@@ -4,7 +4,10 @@ import random
 print()
 print("Was ist dein Name?")
 name = input(">>> ")
-Atacker = {"Sledge":"softbreach","Thatcher":"support"}
+Atacker = {"Sledge":"softbreach","Thatcher":"support","Ash":"softbreach","Thermite":"hardbreach",
+           "Twitch":"gadget","Montagne":"support","Glaz":"atack","Fuze":"gadget","Blitz":"atack",
+           "IQ":"gadget","Buck":"atack","Blackbeard":"atack","Capitao":"atack","Jackel":"gadget",
+           "Ying":"gadget","Zofia":"atack","Doakkaebi":"support","Lion":"support"}
 
 if name == "Alex":
     Angreifer = ["Sledge", "Thermite", "Dokkaebi", "Finka"]
