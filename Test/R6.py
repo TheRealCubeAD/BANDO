f0463950043fb30ac3c6716b21b776c69138f387
cubeAD <alexander.duca@gmail.com>
2018-05-10 18:39:17 +0200

    R6

---
+++ Test/R6.py
@@ -0,0 +1,24 @@
+
+import random
+
+print()
+print("Was ist dein Name?")
+name = input(">>> ")
+
+if name == "Alex":
+    Angreifer = ["Sledge", "Thermite", "Dokkaebi", "Finka"]
+    Verteidiger = ["Smoke", "Mute", "Frost"]
+elif name == "Benno":
+    Angreifer = ["Sledge", "Blitz", "Hibana", "Jackal", "Ying"]
+    Verteidiger = ["Smoke", "Tachanka", "Caveira", "Echo"]
+
+print()
+print("Angreifer: 'A', Verteidiger: 'V'")
+
+while True:
+    print()
+    i = input(">>> ")
+    if i == "A":
+        print(random.choice(Angreifer))
+    elif i == "V":
+        print(ramdom.choice(Verteidiger))
