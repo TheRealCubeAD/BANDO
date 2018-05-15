d4451923f0f9889000ff4ff3d3fa04e7d416384f
Benno <bennington.schaab@gmail.com>
2018-05-15 20:18:49 +0200

    B

--- Crap/shellshock.py
+++ Crap/shellshock.py
@@ -9,7 +9,6 @@ tolerance = 0.85
 def is_green(pix):
     per = ((255-pix[0])/255 + pix[1]/255 + (255-pix[2])/255)/3
     if per > tolerance:
-        print(pix,per)
         return True
     else:
         return False
@@ -17,27 +16,24 @@ def is_green(pix):
 def is_red(pix):
     per = ( pix[0] / 255 + (255-pix[1]) / 255 + (255 - pix[2]) / 255) / 3
     if per > tolerance:
-        print(pix,per)
         return True
     else:
         return False
 
-time.sleep(5)
-pic = ImageGrab.grab()
-print("PIC grabbed")
-sx,sy = pic.size
-matrix = [[None for _ in range(sx)] for __ in range(sy)]
-print("Matrix created")
-for y in range(sy):
-    for x in range(sx):
-        pixel = pic.getpixel((x,y))
-        if is_green(pixel):
-            print("green at",y,x)
-        elif is_red(pixel):
-            print("red at",y,x)
-        else:
-            gr = int(sum(pixel)/3)
-            pic.putpixel((x,y),(gr,gr,gr))
-print("Matrix red")
-pic.show()
+def run():
+    pic = ImageGrab.grab()
+    sx, sy = pic.size
+    player = []
+    enemy = None
+    for y in range(sy):
+        for x in range(sx):
+            pixel = pic.getpixel((x,y))
+            if is_green(pixel):
+                player.append((x,y))
+            elif is_red(pixel) and not enemy == None:
+                enemy = (x,y)
+    playerkord = (int(sum([i[0] for i in player])/len(player)),int(sum([i[1] for i in player])/len(player)))
+    print(playerkord)
 
+while 1:
+    run()
