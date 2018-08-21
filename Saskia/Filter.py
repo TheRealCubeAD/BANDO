58a964a89085e33a99d31e4fc9e44e57b3c29ef0
Benno <bennington.schaab@gmail.com>
2018-08-21 15:27:28 +0200

    Filter

--- Saskia/Filter.py
+++ Saskia/Filter.py
@@ -23,8 +23,8 @@ for xx in range(x):
         pixH = hint.getpixel((xx,yy))
         pixR = (pixT[0]+pixH[0],pixT[1]+pixH[1],pixT[2]+pixH[2])
 
-        if pixT[0] != 255:
-            mi = min(pixR[0],pixR[1],pixR[2])
+        if pixT[0] < 240:
+            mi = int(min(pixR[0],pixR[1],pixR[2])/3)
             pixR = (pixR[0]-mi,pixR[1]-mi,pixR[2]-mi)
 
         res.putpixel((xx,yy),pixR)
