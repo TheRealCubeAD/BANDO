b9bd4ad7c2dc85a111e1e419b91451687400d513
Benno <bennington.schaab@gmail.com>
2018-08-21 15:04:36 +0200

    Filter

--- Saskia/Filter.py
+++ Saskia/Filter.py
@@ -1,14 +1,13 @@
 from PIL import Image
 
 
-path1 = "Saskia.png"  #text
-path2 = "Text.png"  #hintergrund
+path1 = "C:/Users/benni/PycharmProjects/BANDO/Saskia/Saskia.png"  #text
+path2 = "C:/Users/benni/PycharmProjects/BANDO/Saskia/B20001.png"  #hintergrund
 path3 = "RES.png"  #ergebniss
 
 
 text = Image.open(path2)
 hint = Image.open(path1)
-
 
 x,y = text.size
 verd = 50
@@ -22,13 +21,10 @@ for xx in range(x):
 
         pixT = text.getpixel((xx,yy))
         pixH = hint.getpixel((xx,yy))
-
         pixR = (pixT[0]+pixH[0],pixT[1]+pixH[1],pixT[2]+pixH[2])
 
         if pixT[0] != 255:
             pixR = (pixR[0]-verd,pixR[1]-verd,pixR[2]-verd)
-        else:
-            pixR = (pixR[0] - verd2, pixR[1] - verd2, pixR[2] - verd2)
 
         res.putpixel((xx,yy),pixR)
 
