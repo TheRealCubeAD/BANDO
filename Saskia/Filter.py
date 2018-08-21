d17046bbfc95462404b0929070da4cf231e097f4
cubeAD <alexander.duca@gmail.com>
2018-08-21 15:31:27 +0200

    Saskia

--- Saskia/Filter.py
+++ Saskia/Filter.py
@@ -1,9 +1,9 @@
 from PIL import Image
 
 
-path1 = "C:/Users/benni/PycharmProjects/BANDO/Saskia/Saskia.png"  #text
-path2 = "C:/Users/benni/PycharmProjects/BANDO/Saskia/B20001.png"  #hintergrund
-path3 = "RES.png"  #ergebniss
+path1 = "Saskia.png"  #text
+path2 = "B20001.png"  #hintergrund
+path3 = "RES2.png"  #ergebniss
 
 
 text = Image.open(path2)
@@ -24,7 +24,7 @@ for xx in range(x):
         pixR = (pixT[0]+pixH[0],pixT[1]+pixH[1],pixT[2]+pixH[2])
 
         if pixT[0] < 240:
-            mi = int((min(pixR[0],pixR[1],pixR[2])*2)/3)
+            mi = int(min(pixR[0],pixR[1],pixR[2])/2)
             pixR = (pixR[0]-mi,pixR[1]-mi,pixR[2]-mi)
 
         res.putpixel((xx,yy),pixR)
