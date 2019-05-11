7bf31cfd903f3c188fb7f942ff9e726c613f8267
Alex <alexander.duca@gmail.com>
2019-05-11 14:44:54 +0200

    UPDATE

--- Alex' Projekte/Brainfire/Prototyp_Brainfire.py
+++ Alex' Projekte/Brainfire/Prototyp_Brainfire.py
@@ -8,7 +8,6 @@ LILA = '\033[95m'
 END = '\033[0m'
 POINT = "●"
 
-
 # Gibt n Leerzeilen aus
 def newline(n):
     for i in range(n):
@@ -17,9 +16,11 @@ def newline(n):
 # Gibt Matrix in Konsole aus
 def printMatrix( matrix ):
     newline(1)
-    for i in range(len(matrix)):
-        reihe = matrix[i]
-        print(reihe)
+    for reihe in matrix:
+        s = ""
+        for i in range(len(reihe)):
+            s += str(reihe[i]) + " "
+        print(s)
     newline(1)
 
 # Lost mit einer Wahrscheinlichkeit von p1 die Zahl 1 aus.
@@ -32,6 +33,10 @@ def randomBool( p1 ):
 def getZelle(matrix,pos):
     return matrix[pos[1]][pos[0]]
 
+def setZelle(matrix,pos,content):
+    matrix[pos[1]][pos[0]] = content
+    return matrix
+
 # Generiert ein Level
 def generiereLevel( pSteine ):
     Level = [[0 for _ in range(8)] for _ in range(8)]
@@ -41,16 +46,6 @@ def generiereLevel( pSteine ):
 
     return Level
 
-endingPos = None
-visitedNodes = None
-directions = ["up","down","left","right"]
-
-def startLevelTest(level):
-    global endingPos,visitedNodes
-    startingPos = [3,7]
-    endingPos = [4,0]
-    visitedNodes = []
-    return testLevel(level,startingPos,[])
 
 def laufen(level,pos,richtung):
     iRichtung = {"up":(0,-1),"down":(0,1),"left":(-1,0),"right":(1,0)}
@@ -62,6 +57,33 @@ def laufen(level,pos,richtung):
         return pos
     else:
         return laufen(level,npos,richtung)
+
+
+
+endingPos = None
+visitedNodes = None
+directions = ["up","down","left","right"]
+
+def startLevelTest(level):
+    global endingPos,visitedNodes
+    startingPos = [3,7]
+    endingPos = [4,0]
+    visitedNodes = []
+
+    levelBunt = deepcopy(level)
+    path = testLevel(level,startingPos,[])
+    if path != None:
+        for pos in path:
+            content = getZelle(level, pos)
+            levelBunt = setZelle(levelBunt,pos, GREEN + str(content) + END)
+    newline(2)
+    printMatrix(levelBunt)
+    newline(1)
+    print(path)
+    newline(2)
+    return path
+
+
 
 
 def testLevel(level,pos,path):
@@ -83,12 +105,6 @@ def testLevel(level,pos,path):
 
 
 
-
-
-
-
 while True:
-    level = generiereLevel(0.2)
-    printMatrix(level)
-    print(startLevelTest(level))
-    input()
+    pSteine = float(input(">>> "))
+    startLevelTest(generiereLevel(pSteine))
