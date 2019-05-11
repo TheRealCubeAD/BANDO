37730b4ab8a58accdcaf09c3e10fb40f04a25b3d
Alex <alexander.duca@gmail.com>
2019-05-11 13:24:18 +0200

    UPDATE

---
+++ Alex' Projekte/Brainfire/Prototyp_Brainfire.py
@@ -0,0 +1,34 @@
+# Gott würfelt nicht
+import random
+
+# Gibt n Leerzeilen aus
+def newline(n):
+    for i in range(n):
+        print()
+
+# Gibt Matrix in Konsole aus
+def printMatrix( matrix ):
+    newline(1)
+    for i in range(len(matrix)):
+        reihe = matrix[i]
+        print(reihe)
+    newline(1)
+
+# Lost mit einer Wahrscheinlichkeit von p1 die Zahl 1 aus.
+def randomBool( p1 ):
+    if random.random() < p1:
+        return 1
+    else:
+        return 0
+
+# Generiert ein Level
+def generiereLevel( pSteine ):
+    Level = [[0 for _ in range(8)] for _ in range(8)]
+    for Zeile in Level:
+        for i in range(len(Zeile)):
+            Zeile[i] = randomBool( pSteine )
+    return Level
+
+while True:
+    printMatrix(generiereLevel(0.2))
+    input()
