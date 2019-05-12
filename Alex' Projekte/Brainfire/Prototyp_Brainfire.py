4ea837083da6b37ebb4f2284ebdccd9a4c65e7b7
Alex <alexander.duca@gmail.com>
2019-05-12 15:19:42 +0200

    KOMMENTARE UPDATE

--- Alex' Projekte/Brainfire/Prototyp_Brainfire.py
+++ Alex' Projekte/Brainfire/Prototyp_Brainfire.py
@@ -1,10 +1,15 @@
-# Importware
+"""
+Importware
+"""
 import math # <3
 import random # Gott würfelt nicht
 from copy import deepcopy # copy aber deep
+import time
 
 
-# Definitionen der farbigen Outputs
+"""
+Definiert die Farbwerte für die Ausgabe
+"""
 RED = '\033[91m'
 BLUE = '\033[94m'
 GREEN = '\033[92m'
@@ -14,26 +19,40 @@ POINT = "●"
 
 
 # Defintion der Dimension des Spielbretts
-feldBreite = 8 # > 0
-feldHoehe = 8 # > 0
+feldBreite = None # > 0
+feldHoehe = None # > 0
+startingPos = None
+endingPos = None
+"""
+Setzt die Dimension des Spielbretts fest.
+Legt anschließend Start- und Endposition fest.
+"""
+def setzeFeldgroesse(nBreite, nHoehe):
+    global feldBreite, feldHoehe, startingPos, endingPos
+
+    # Defintion der Dimension des Spielbretts
+    feldBreite = nBreite  # > 0
+    feldHoehe = nHoehe  # > 0
+
+    if feldBreite % 2 == 0:
+        startingPos = [math.floor(feldBreite / 2) - 1, feldHoehe - 1]
+        endingPos = [math.floor(feldBreite / 2), 0]
+    else:
+        startingPos = [math.floor(feldBreite / 2), feldHoehe - 1]
+        endingPos = [math.floor(feldBreite / 2), 0]
 
 
-# Start- und Zielpunkt
-if feldBreite % 2 == 0:
-    startingPos = [ math.floor(feldBreite/2)-1 , feldHoehe-1 ]
-    endingPos = [ math.floor(feldBreite/2) , 0 ]
-else:
-    startingPos = [math.floor(feldBreite / 2), feldHoehe - 1]
-    endingPos = [math.floor(feldBreite / 2), 0]
-
-
-
-# Gibt n Leerzeilen aus
+"""
+Gibt n Leerzeile in der Konsole aus.
+"""
 def newline(n):
     for i in range(n):
         print()
 
-# Gibt Matrix in Konsole aus
+
+"""
+Gibt eine Matrix "schön" in der Konsole aus.
+"""
 def printMatrix( matrix ):
     newline(1)
     for reihe in matrix:
@@ -43,23 +62,35 @@ def printMatrix( matrix ):
         print(s)
     newline(1)
 
-# Lost mit einer Wahrscheinlichkeit von p1 die Zahl 1 aus.
+
+"""
+Gibt mit einer Wahrscheinlichkeit von p1 den Wert 1 zurück, ansonsten 0.
+"""
 def randomBool( p1 ):
     if random.random() < p1:
         return 1
     else:
         return 0
 
-# Bestimmt Inhalt einer Zelle
+
+"""
+Gibt den Inhalt einer Zelle in einer Matrix zurück.
+"""
 def getZelle(matrix,pos):
     return matrix[pos[1]][pos[0]]
 
-# Setzt den Inhalt einer Zelle fest
+
+"""
+Gibt eine Matrix zurück, die an der Position pos mit dem Wert content überschrieben wurde.
+"""
 def setZelle(matrix,pos,content):
     matrix[pos[1]][pos[0]] = content
     return matrix
 
-# Generiert ein Level
+
+"""
+Generiert ein Feld mit einem "Stein zu Fläche"-Verhältnis von pSteine.
+"""
 def generiereLevel( pSteine ):
     level = [[0 for _ in range(feldBreite)] for _ in range(feldHoehe)]
     for Zeile in level:
@@ -77,7 +108,10 @@ def generiereLevel( pSteine ):
 
     return level
 
-# Bestimmt die naechste erreichbare Position von einem bestimmten Punkt aus
+
+"""
+Bestimmt die naechste erreichbare Position von einem bestimmten Punkt in einem Level aus.
+"""
 def laufen(level,pos,richtung):
     iRichtung = {"up":(0,-1),"down":(0,1),"left":(-1,0),"right":(1,0)}
     ix,iy = iRichtung[richtung]
@@ -89,7 +123,12 @@ def laufen(level,pos,richtung):
     else:
         return laufen(level,npos,richtung)
 
-# Entfernt das Element einer Listenliste, das pos als erstes Element hat.
+
+
+"""
+Ergebnislisten-Bearbeitung:
+Entfernt das Element einer Listenliste, das pos als erstes Element hat.
+"""
 def posEntfernen(liste, pos):
     for Eintrag in liste:
         if Eintrag[0] == pos:
@@ -97,24 +136,39 @@ def posEntfernen(liste, pos):
             return liste
     return liste
 
-# Überprüft, ob ein Element in einer Listenliste pos als erstes Element hat.
+"""
+Ergebnislisten-Bearbeitung:
+Überprüft, ob ein Element in einer Listenliste pos als erstes Element hat.
+"""
 def posExistiert(liste, pos):
     for Eintrag in liste:
         if Eintrag[0] == pos:
             return True
     return False
 
-# Gibt das zweite Element eines Elements einer Listenliste aus, das als erstes Element pos hat.
+"""
+Ergebnislisten-Bearbeitung:
+Gibt das zweite Element eines Elements einer Listenliste aus, das als erstes Element pos hat.
+"""
 def posPfad(liste, pos):
     for Eintrag in liste:
         if Eintrag[0] == pos:
             return Eintrag[1]
     return None
 
+
+
+"""
+Gibt die seit dem angegebenen Zeitpunkt vergangene Zeit zurück.
+"""
+def vergangeneZeit(zeitpunkt):
+    return time.time() - zeitpunkt
+
+
+Buchstabenliste = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 """
 Wandelt ein Koordinatentupel in die im Schach übliche Notation um.
 """
-Buchstabenliste = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 def schachnotation( tupel ):
     x = Buchstabenliste[ tupel[0] ]
     y = str( feldHoehe - tupel[1] )
@@ -248,16 +302,42 @@ def testLevel(level,pos):
         return [pos] + besterPfad
 
 
-# Das ist das Verhältnis zwischen Steine und Fläche aus dem Original-Pokemon-Spiel: 13/168
-pSteine = float(13/168)
+"""
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+"""
 
+
+"""
+Parameterauswahl
+"""
+# Das ist das Verhältnis zwischen Steine und Fläche aus dem Original-Pokemon-Spiel: 13/168
+pSteine = float(13/168) # Stein zu Fläche - Verhältnis
+versuchsZeit = 1 #Sekunden # Anzahl der Sekunden, die das Programm höchstens rechnen soll
+mindestlaengeLoesung = 11 # Mindestlänge des Lösungswegs
+setzeFeldgroesse(8,8) # Größe des Felds
+
+
+"""
+Versuchsdurchführung
+"""
+startTime = time.time() # Setze den Timer auf
 path = None
-while path == None:
+while path == None and vergangeneZeit(startTime) < versuchsZeit:
     level = generiereLevel(pSteine)
     path = startLevelTest(level)
     if path != None:
-        if len(path) < (feldBreite + feldHoehe):
+        if len(path) < mindestlaengeLoesung:
             path = None
-zeichneLevelMitLoesung(level, path)
-print("Länge des (kürzesten) Lösungspfads:",len(path))
+
+
+"""
+Ergebnisausgabe
+"""
+newline(1)
+if path == None:
+    print(None)
+else:
+    zeichneLevelMitLoesung(level, path)
+    print("Länge des (kürzesten) Lösungspfads:", len(path))
+    print("Zeit zur Generierung:", vergangeneZeit(startTime))
 newline(1)
