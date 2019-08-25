2952b6f1daa8716057c3365c16f318958b133806
TheRealCubeAD <}NS?s#cw[XI4WW(W>
2019-08-25 22:03:43 +0200

    Testpush

--- Brainfire2/brainfire_paths.py
+++ Brainfire2/brainfire_paths.py
@@ -1,9 +1,12 @@
 
+def space():
+    print()
 
-
-
+space
 print("Wie viele Felder ist der Raum hoch?")
 hoehe = int(input(">>> "))
-print()
+space()
 print("Wie viele Felder ist der Raum breit?")
 breite = int(input(">>> "))
+space()
+
