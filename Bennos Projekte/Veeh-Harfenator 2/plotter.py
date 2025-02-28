c69b66b9b1c9d2d29f9e8dbab19f61c462ee1e8b
Benjamin Schaab <benno.schaab@gmail.com>
2025-02-28 10:22:59 +0100

    VeH2: plotter notes and lines

--- Bennos Projekte/Veeh-Harfenator 2/plotter.py
+++ Bennos Projekte/Veeh-Harfenator 2/plotter.py
@@ -10,15 +10,39 @@ max_notes_per_site = 51
 lng = None
 
 def plott(song, grid=False):
+    """
+    Plots the song
+    :param song: Song object
+    :param grid: Show grid
+    """
     sites = calc_notes(song)
     for site in sites:
         plott_site(site, grid)
 
 def plott_site(entrys, grid):
+    """
+    Plots a site
+    :param entrys: List of notes
+    :param grid: Show grid
+    """
     image = Image.new("1", (size, size), 1)
     draw = ImageDraw.Draw(image)
+
+    # Grid
     if grid:
         draw_grid(draw, len(entrys))
+
+    # Vert lines
+    for ey in range(len(entrys) - 1):
+        n1 = entrys[ey]
+        n2 = entrys[ey + 1]
+        y1 = calc_y(len(entrys), ey)
+        y2 = calc_y(len(entrys), ey + 1)
+        x1 = calc_x(n1.notes[0])
+        x2 = calc_x(n2.notes[0])
+        draw.line(((x1, y1), (x2, y2)), width=5)
+
+    # Notes and horizontal lines
     for ey in range(len(entrys)):
         notes = entrys[ey].notes
         y = calc_y(len(entrys), ey)
@@ -27,11 +51,75 @@ def plott_site(entrys, grid):
             ex2 = notes[i + 1]
             if ex1 == None or ex2 == None:
                 continue
-            draw.line(((calc_x(ex1), y), (calc_x(ex2), y)), width=5)
+            draw.line(((calc_x(ex1), y), (calc_x(ex2), y)), width=3)
+        draw_note(draw, entrys[ey], y)
     image.show()
 
 
+def draw_note(draw, note, y):
+    """
+    Draws a note
+    :param draw: draw object
+    :param note: Note
+    :param y: y position
+    """
+
+    sizes = {1: 20, 2: 15, 4: 15, 8: 10}
+    size = sizes[note.lenght]
+    if note.lenght == 1 or note.lenght == 2:
+        hollow = True
+    else:
+        hollow = False
+
+    for n in note.notes:
+        if n == None:
+            continue
+        x = calc_x(n)
+        if note.mode == 0:
+            draw_ellipse(draw, x, y, size, hollow)
+        else:
+            draw_rect(draw, x, y, size, hollow)
+
+        if note.point:
+            draw.circle((x+size*1.5, y+size), 5, fill=0)
+
+
+def draw_ellipse(draw, x, y, size, hollow=False):
+    """
+    Draws an ellipse
+    :param draw: draw object
+    :param x: x position
+    :param y: y position
+    :param size: size
+    :param hollow: hollow
+    """
+    stretch = 1.5
+    if hollow:
+        draw.ellipse(((x - size*stretch, y - size), (x + size*stretch, y + size)), outline=0, fill=1, width=5)
+    else:
+        draw.ellipse(((x - size*stretch, y - size), (x + size*stretch, y + size)), fill=0)
+
+def draw_rect(draw, x, y, size, hollow=False):
+    """
+    Draws a rectangle
+    :param draw: draw object
+    :param x: x position
+    :param y: y position
+    :param size: size
+    :param hollow: hollow
+    """
+    stretch = 0.75
+    if hollow:
+        draw.rectangle(((x - size*stretch, y - size), (x + size*stretch, y + size)), outline=0, fill=1, width=5)
+    else:
+        draw.rectangle(((x - size*stretch, y - size), (x + size*stretch, y + size)), fill=0)
+
 def draw_grid(draw, count_y):
+    """
+    Draws the grid
+    :param draw: draw object
+    :param count_y: count of y lines
+    """
     font = ImageFont.truetype(font="arial.ttf", size=30)
     for i in range(max_note + 1):
         x = calc_x(i)
@@ -39,8 +127,8 @@ def draw_grid(draw, count_y):
     draw.rectangle(((0, 25), (size, 60)), fill="white")
     for i in range(max_note + 1):
         x = calc_x(i)
-        text_size = draw.textsize(lng.keys[i], font=font)
-        draw.text((x - text_size[0]/2, 25), lng.keys[i], font=font)
+        text_size = draw.textlength(lng.keys[i], font=font)
+        draw.text((x - text_size/2, 25), lng.keys[i], font=font)
     for i in range(count_y):
         y = calc_y(count_y, i)
         draw.line(((0, y), (size, y)))
@@ -48,6 +136,11 @@ def draw_grid(draw, count_y):
 
 
 def calc_notes(song):
+    """
+    Splits the notes into sites
+    :param song: Song
+    :return: List of sites
+    """
     notes = song.get_notes()
     site_count = int(len(notes) / max_notes_per_site) + 1
     result = [None for _ in range(site_count)]
